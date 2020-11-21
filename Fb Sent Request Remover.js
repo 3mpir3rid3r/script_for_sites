990055db7fc2699403735517ad6af17ef9f32fed
3mpir3rid3r <35126851+3mpir3rid3r@users.noreply.github.com>
2020-11-21 20:58:12 +0530

    Update and rename Fb Request Remover.js to  Fb Sent Request Remover.js

---
+++ Fb Sent Request Remover.js
@@ -0,0 +1,22 @@
+function scrollToBottom() {
+        scrollingElement.scrollTop = scrollingElement.scrollHeight;
+    }
+    function runProcess() {
+        setTimeout(function () {
+            var elms = document.querySelectorAll('button._54k8._52jg._56bs._26vk._56b_._5uc3._3cqr._8yo0._56bt');
+            if (elms && elms.length > 0) {
+                console.error('Fetch data from page ' + elms.length);
+                for (; i < elms.length; i++) {
+                    console.error('Fetch data from page ' + elms.length);
+                    elms[i].click();
+                }
+            } else {
+                window.reload();
+            }
+            scrollToBottom();
+            runProcess();
+        }, 5000)
+    }
+    var i = 0;
+    var scrollingElement = (document.scrollingElement || document.body);
+    runProcess();
