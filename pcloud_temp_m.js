0b1b6f6dfb83fabf9676720eae48da073168d808
3mpir3rid3r <35126851+3mpir3rid3r@users.noreply.github.com>
2020-12-14 20:59:29 +0530

    Update pcloud_temp_m.js

--- pcloud_temp_m.js
+++ pcloud_temp_m.js
@@ -1,117 +1,112 @@
 // ==UserScript==
-// @name         SEEDR
+// @name         pcloud uploader
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        *://www.seedr.cc/*
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @match        https://my.pcloud.com/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.12.3/sweetalert2.min.js
 // @resource     SweetAlert2CSS https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.12.3/sweetalert2.min.css
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-    GM_getResourceText('SweetAlert2CSS');
-    GM_addStyle(' .mr-3{margin-right: 3em;} .br{border-radius: 10px; padding: 3px 17px !important;} .swal2-actions{ margin: 0 !important;} ');
+    GM_addStyle(GM_getResourceText('SweetAlert2CSS'));
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
 
-    const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-            confirmButton: 'btn btn-info mr-3 br',
-            cancelButton: 'btn btn-info br'
-        },
-        buttonsStyling: false
-    });
+    function randomNumber() {
+        var val = Math.floor(Math.random() * (19 - 6 + 1) + 6);
+        val = (val === 13) ? randomNumber() : val;
+        return val;
+    }
 
-    function viewLinks(data){
-        swalWithBootstrapButtons.fire({
-            text: 'Yore links is here.you can copy it.',
+    function randomHash() {
+        var midle = Math.floor(Math.random() * (16900000 - 16000000 + 1) + 16000000);
+        var last = Math.floor(Math.random() * (999 - 300 + 1) + 300);
+
+        return 'upload-' + midle + '-xhr-' + last;
+    }
+
+    $(document).on('click', '#link', function () {
+        Swal.fire({
+            title: 'Paste Links',
+            html: '<span>paste your links here.line breack consider as new link.</span><br><textarea id="swal-input1" class="swal2-input" style=" height: 250px; font-size: 15px; "></textarea>',
             width: 1250,
             showCancelButton: true,
-            html: '<span>Yore links is here.you can copy it.</span><br><textarea id="swal-input1" class="swal2-input" style=" height: 250px; font-size: 15px; ">'+data+'</textarea>',
-            confirmButtonText: 'Copy',
-            allowOutsideClick: false,
-            onOpen: function(el) {
-                var input = $('#swal-input1');
-                input.select();
-                input.prop('readonly', true);
-
-                var container = $(el);
-                var originalConfirmButton = container.find('.swal2-confirm');
-                var clonedConfirmButton = originalConfirmButton.clone();
-
-                originalConfirmButton.hide();
-                clonedConfirmButton.insertAfter(originalConfirmButton);
-
-                clonedConfirmButton.on('click', function() {
-                    var input = $('#swal-input1');
-                    input.select();
-                    input.prop('readonly', true);
-                    document.execCommand("copy")
-                });
-            },
-
-        });
-    }
-
-    function getFoldersLinks(){
-        var links='';
-        var count=0;
-        var list=$('div[folder_id][folder_id!="-1"]');
-        $.each(list,function () {
-            $.post("https://www.seedr.cc/content.php?action=create_empty_archive", {
-                folder_id: 4565362,
-                archive_arr:'[{"type":"folder","id":'+$(this).attr('folder_id').toString()+'}]'
-            }).done(function (data) {
-                if ((count + 1) == list.length) {
-                    links = links + data.archive_url+'\n'
-                    viewLinks(links);
-                } else {
-                    count = count + 1;
-                    links = links + data.archive_url+'\n'
-                }
-            });
-        });
-    }
-
-    function getFilesLinks(){
-        var links='';
-        var count=0;
-        var list=$('div[folder_file_id]');
-        $.each(list,function () {
-            $.post("https://www.seedr.cc/content.php?action=fetch_file", {
-                folder_file_id: $(this).attr('folder_file_id').toString()
-            }).done(function (data) {
-                if ((count + 1) == list.length) {
-                    links = links + data.url+'\n'
-                    viewLinks(links);
-                } else {
-                    count = count + 1;
-                    links = links + data.url+'\n'
-                }
-
-            });
-        });
-    }
-
-    $(document).on('click','#links',function(){
-        swalWithBootstrapButtons.fire({
-            text: "Select what you need!",
-            showCancelButton: true,
-            confirmButtonText: 'For All Folder',
-            cancelButtonText: 'For All Files',
-            focusConfirm: false
+            confirmButtonText: 'Upload',
+            showLoaderOnConfirm: true,
+            preConfirm: () => {
+                return [
+                    document.getElementById('swal-input1').value
+                ]
+            }
         }).then((result) => {
             if (result.isConfirmed) {
-                getFoldersLinks();
-            } else if (result.dismiss === Swal.DismissReason.cancel) {
-                getFilesLinks();
+                if (result.value) {
+                    let auth = readCookie("pcauth");
+                    var b = $.trim(result.value.toString()).split("\n");
+                    Swal.fire({
+                        title: 'Uploading..',
+                        text: 'Feched ' + b.length + ' links.Processing..'
+                    })
+                    Swal.showLoading();
+
+                    var count = 0;
+                    var flag = true;
+                    $.each(b, function (i, v) {
+                        setTimeout(function () {
+                            var hash = randomHash();
+                            if (flag) {
+                                $.post("https://api" + randomNumber() + ".pcloud.com/downloadfile", {
+                                    folderid: 0,
+                                    progresshash: hash,
+                                    nopartial: 1,
+                                    url: v,
+                                    auth: auth
+                                },function (params) {
+
+                                }).done(function (data) {
+                                    console.log(data);
+                                    if (data.result === 0) {
+                                        var count = Swal.getContent().textContent;
+                                        var currentCount = parseInt((count.split("/")[0].indexOf('Processing..')> -1 ) ? 0 : count.split("/")[0]);
+                                        if ((currentCount + 1) == b.length) {
+                                            Swal.getTitle().textContent = 'Done.'
+                                            Swal.getContent().textContent = (currentCount + 1) + "/" + b.length;
+                                            Swal.hideLoading();
+                                        } else {
+                                            Swal.getContent().textContent = (currentCount + 1) + "/" + b.length;
+                                        }
+                                    } else {
+                                        Swal.getTitle().textContent = 'Faild.'
+                                        $('#swal2-content').html('<span style="color:red" >test</span>');
+                                        Swal.getContent().textContent = data.error;
+                                        Swal.hideLoading();
+                                        flag = false;
+                                    }
+
+                                });
+                            }
+                        }, count * 4000);
+                        count++;
+                    })
+                }
             }
-        });
+        })
     });
 
-    $('#add-folder-button').parent('div').prepend('<button  id="links"  style="float:right;padding: 5px;    float: right;    display: block;    line-height: 12px;    margin-right: 35px;    background: none;    color: black;    outline: none;" class="radius"><i class="fa fa-copy"></i> Get Links</button>');
-
+    setTimeout(function () {
+        $('.gridmanage').prepend("<div id='link' class='uploadfiles'>Upload All</div>");
+    }, 6000);
 })();
