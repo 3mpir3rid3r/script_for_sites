9ec2427e89f6e6e0683888aa96f1c0e6a0918244
3mpir3rid3r <35126851+3mpir3rid3r@users.noreply.github.com>
2020-12-23 19:30:36 +0530

    Update pcloud_temp_m.js

--- pcloud_temp_m.js
+++ pcloud_temp_m.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pcloud uploader
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://my.pcloud.com/*
